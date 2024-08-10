62b44d02f534285827f8c9e00e0065a8cd3ccda8
joffre <joffre.hermosilla@gmail.com>
2024-08-10 10:06:58 -0500

    [agregator patron][sec01][Finalizado]

--- src/main/java/coderpath/spectro/DemoApplication.java
+++ src/main/java/coderpath/spectro/DemoApplication.java
@@ -3,7 +3,9 @@ package coderpath.spectro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(scanBasePackages = "coderpath.spectro.sec01")
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/coderpath/spectro/sec01/dto/Review.java
@@ -0,0 +1,17 @@
+package coderpath.spectro.sec01.dto;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Review {
+
+	
+
+    private Integer id;
+    private String user;
+    private Integer rating;
+    private String comment;
+}
